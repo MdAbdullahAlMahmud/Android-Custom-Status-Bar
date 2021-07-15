e68156c33440dfe361cb7c4dd9b7d0d44ae3cda4
MeticulousMunna1 <mmunna864@gmail.com>
2021-07-15 15:13:56 +0600

    custom status bar module

---
+++ CustomStatusBar/src/main/java/com/mkrlabs/customstatusbar/CustomStatusBar.java
@@ -0,0 +1,86 @@
+package com.mkrlabs.customstatusbar;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.annotation.ColorRes;
+
+public class CustomStatusBar {
+    public static void makeFullScreen(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = activity.getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+
+    public static void setSystemBarColor(Activity act, @ColorRes int color, boolean is_iconColorBlack) {
+
+
+        int nightModeFlags =
+                act.getResources().getConfiguration().uiMode &
+                        Configuration.UI_MODE_NIGHT_MASK;
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                is_iconColorBlack = false;
+                break;
+
+            case Configuration.UI_MODE_NIGHT_NO:
+                is_iconColorBlack = true;
+                break;
+
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+
+                break;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = act.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(act.getResources().getColor(color));
+        }
+        if (is_iconColorBlack) {
+            setSystemBarLight(act);
+        }
+
+    }
+
+    private static void setSystemBarLight(Activity act) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View view = act.findViewById(android.R.id.content);
+            int flags = view.getSystemUiVisibility();
+            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            view.setSystemUiVisibility(flags);
+        }
+    }
+
+
+    public static void systembartransparent(Activity activity) {
+        //make translucent statusBar on kitkat devices
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        //make fully Android Transparent Status bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+}
